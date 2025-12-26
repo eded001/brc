2db266f4f2d4ea1aaa1ac46be701a914f8f5cb6f
Ed <edgarams09@gmail.com>
2025-12-26 17:01:40 -0300

    feat: add new screens

---
+++ src/features/auth/screens/ForgotPassword.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ForgotPassword() {
+    return (
+        <View className="flex-1 items-center justify-center bg-black">
+            <Text className="text-green-500 text-3xl font-bold">ForgotPassword</Text>
+        </View>
+    );
+}
