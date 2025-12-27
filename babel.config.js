37b2b10891577cd979a694c72c007470b365b57b
Ed <edgarams09@gmail.com>
2025-12-27 16:55:56 -0300

    chore: add alias

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          '@': './src',
+          '@': './src/shared',
           '@screens': './src/features',
           '@navigation': './src/app/navigation'
         },
