6e732e38f838a8fc249c85faea7d7ff63208894d
Ed <edgarams09@gmail.com>
2025-12-26 22:19:00 -0300

    feat: add barrel patterns

---
+++ src/features/auth/index.ts
@@ -0,0 +1,5 @@
+import ForgotPassword from "./screens/ForgotPassword";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+
+export { Login, Register, ForgotPassword };
