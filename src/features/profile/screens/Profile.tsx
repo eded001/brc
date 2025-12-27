5e87c35782668a6dc784e703bb308e3337673ca2
Ed <edgarams09@gmail.com>
2025-12-26 22:21:03 -0300

    chore: change folder directory

--- src/features/profile/screens/Profile.tsx
+++ src/features/profile/screens/Profile.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function Profile() {
     return (
         <View className="flex-1 items-center justify-center bg-black">
-            <Text className="text-blue-500 text-3xl font-bold">Profile</Text>
+            <Text className="text-green-500 text-3xl font-bold">Profile</Text>
         </View>
     );
 }
