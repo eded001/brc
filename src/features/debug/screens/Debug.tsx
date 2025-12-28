2197c4441d0cfc7902200f7b7a68e5e93d40903b
Ed <edgarams09@gmail.com>
2025-12-28 01:28:14 -0300

    feat: using the goto component

--- src/features/debug/screens/Debug.tsx
+++ src/features/debug/screens/Debug.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { View, Text } from 'react-native';
 
-import { RootStackProps } from '@navigation/types/rootStack';
+// components
+import { GoToScreen } from '@/components/goToScreen';
 
 export default function Test() {
-    const navigation = useNavigation<NativeStackNavigationProp<RootStackProps>>();
-
     return (
         <View className="flex-1 bg-black items-center justify-center px-6">
             <Text className="text-green-500 text-3xl font-bold mb-8">Test Hub</Text>
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Settings')}
+            <GoToScreen
+                screen="Register"
                 className="w-full bg-green-600 py-4 rounded-xl mb-4"
-                activeOpacity={0.8}
             >
-                <Text className="text-black text-center text-lg font-semibold">Explore</Text>
-            </TouchableOpacity>
+                <Text className="text-black text-center text-lg font-semibold">Register</Text>
+            </GoToScreen>
         </View>
     );
 }
