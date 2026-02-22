c15a6f470ce2abcccd529e022bef20273af4bc0f
Ed <108343328+eded001@users.noreply.github.com>
2026-02-22 17:22:25 -0300

    feat: debug and refactor

--- src/app/navigation/RootStack.tsx
+++ src/app/navigation/RootStack.tsx
@@ -2,22 +2,44 @@ import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // screens
-import { Introduction } from "@screens/introduction";
+// import { Introduction } from "@screens/introduction";
 import { Settings } from "@screens/settings";
+import Welcome from "@test/screens/Welcome";
 import Register from "@test/screens/Register";
 import Auth from "@test/screens/Auth";
 import Firebase from "@test/screens/Firebase";
+import Redux from "@screens/debug/test/screens/Redux";
 
 // navigation
-const Stack = createNativeStackNavigator();
 import AppTabs from "./AppTabs";
+import { store } from "@/store";
+import { Provider } from "react-redux";
+
+const Stack = createNativeStackNavigator();
+
+// Configuração centralizada das telas
+const screens = [
+    // {
+    //     name: "Introduction",
+    //     isRenderProp: true,
+    // },
+
+    { name: "Welcome", component: Welcome },
+    { name: "Register", component: Register },
+    { name: "AppTabs", component: AppTabs },
+    { name: "Settings", component: Settings },
+    { name: "Auth", component: Auth },
+    { name: "Firebase", component: Firebase },
+    { name: "Redux", component: Redux }
+];
 
 export default function RootStack() {
-    const [hasSeenIntroduction, setHasSeenIntroduction] = useState(false);
+    // const [hasSeenIntroduction, setHasSeenIntroduction] = useState(false);
 
     return (
-        <Stack.Navigator>
-            {!hasSeenIntroduction && (
+        <Provider store={store}>
+            <Stack.Navigator initialRouteName="AppTabs">
+                {/* {!hasSeenIntroduction && (
                 <Stack.Screen
                     name="Introduction"
                     options={{ headerShown: false }}
@@ -26,42 +48,23 @@ export default function RootStack() {
                         <Introduction
                             onFinish={() => {
                                 setHasSeenIntroduction(true);
-                                navigation.replace("Register");
+                                navigation.replace("AppTabs");
                             }}
                         />
                     )}
                 </Stack.Screen>
-            )}
+            )} */}
 
-             <Stack.Screen
-                name="Register"
-                component={Register}
-                options={{ headerShown: false }}
-            /> 
 
-            <Stack.Screen
-                name="AppTabs"
-                component={AppTabs}
-                options={{ headerShown: false }}
-            />
-
-            <Stack.Screen
-                name="Settings"
-                component={Settings}
-                options={{ headerShown: false }}
-            />
-
-            <Stack.Screen
-                name="Auth"
-                component={Auth}
-                options={{ headerShown: false }}
-            />
-
-            <Stack.Screen
-                name="Firebase"
-                component={Firebase}
-                options={{ headerShown: false }}
-            />
-        </Stack.Navigator>
+                {screens.map(({ name, component }) => (
+                    <Stack.Screen
+                        key={name}
+                        name={name}
+                        component={component}
+                        options={{ headerShown: false }}
+                    />
+                ))}
+            </Stack.Navigator>
+        </Provider>
     );
 }
