2bf37872d7b96a359c111101d62f0445fecca549
Ed <108343328+eded001@users.noreply.github.com>
2026-04-26 17:16:35 -0300

    chore: remove redux and auth

--- src/app/navigation/RootStack.tsx
+++ src/app/navigation/RootStack.tsx
@@ -6,14 +6,10 @@ import { Introduction } from "@screens/introduction";
 import { Settings } from "@screens/settings";
 import Welcome from "@test/screens/Welcome";
 import Register from "@test/screens/Register";
-import Auth from "@test/screens/Auth";
 import Firebase from "@test/screens/Firebase";
-import Redux from "@screens/debug/test/screens/Redux";
 
 // navigation
 import AppTabs from "./AppTabs";
-import { store } from "@/store";
-import { Provider } from "react-redux";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,18 +20,15 @@ const screens = [
     { name: "Register", component: Register },
     { name: "AppTabs", component: AppTabs },
     { name: "Settings", component: Settings },
-    { name: "Auth", component: Auth },
     { name: "Firebase", component: Firebase },
-    { name: "Redux", component: Redux }
 ];
 
 export default function RootStack() {
     // const [hasSeenIntroduction, setHasSeenIntroduction] = useState(false);
 
     return (
-        <Provider store={store}>
-            <Stack.Navigator initialRouteName="AppTabs">
-                {/* {!hasSeenIntroduction && (
+        <Stack.Navigator initialRouteName="AppTabs">
+            {/* {!hasSeenIntroduction && (
                 <Stack.Screen
                     name="Introduction"
                     options={{ headerShown: false }}
@@ -52,15 +45,14 @@ export default function RootStack() {
             )} */}
 
 
-                {screens.map(({ name, component }) => (
-                    <Stack.Screen
-                        key={name}
-                        name={name}
-                        component={component}
-                        options={{ headerShown: false }}
-                    />
-                ))}
-            </Stack.Navigator>
-        </Provider>
+            {screens.map(({ name, component }) => (
+                <Stack.Screen
+                    key={name}
+                    name={name}
+                    component={component}
+                    options={{ headerShown: false }}
+                />
+            ))}
+        </Stack.Navigator>
     );
 }
