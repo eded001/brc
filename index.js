c7cb7beea213c59f5e0c9f06028c5c29cf39f5e0
Ed <108343328+eded001@users.noreply.github.com>
2026-05-01 00:51:44 -0300

    chore: format

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import "./global.css";
-import messaging from '@react-native-firebase/messaging';
+
+import { getApp } from '@react-native-firebase/app';
+import { getMessaging, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
+
 import notifee, { EventType } from '@notifee/react-native';
+
+const app = getApp();
+const messaging = getMessaging(app);
 
 AppRegistry.registerComponent(appName, () => App);
 
 // FCM background handler
-messaging().setBackgroundMessageHandler(async remoteMessage => {
+setBackgroundMessageHandler(messaging, async remoteMessage => {
     console.log('[FCM] Background message:', remoteMessage);
-    // O FCM já exibe automaticamente se a mensagem tiver "notification" payload
-    // Usar isso apenas para mensagens do tipo "data-only"
+
     await notifee.displayNotification({
         title: remoteMessage.data?.title,
         body: remoteMessage.data?.body,
@@ -24,13 +29,12 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
     });
 });
 
-// Notifee background event (ex: ações de notificação)
+// Notifee background event
 notifee.onBackgroundEvent(async ({ type, detail }) => {
     if (type === EventType.PRESS) {
         console.log('[Notifee] Background press:', detail.notification?.data);
     }
     if (type === EventType.ACTION_PRESS) {
-        const actionId = detail.pressAction?.id;
-        console.log('[Notifee] Action pressed:', actionId);
+        console.log('[Notifee] Action pressed:', detail.pressAction?.id);
     }
 });
