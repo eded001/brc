1a2135b16ec9effe172e6098563a90521e38fa79
Ed <108343328+eded001@users.noreply.github.com>
2026-05-01 00:50:28 -0300

    feat: update go to screen component

--- src/shared/components/go-to-screen/GoToScreen.tsx
+++ src/shared/components/go-to-screen/GoToScreen.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { RootStackProps } from "@navigation/types/rootStack";
+import { RootStackProps } from "@/constants/types/rootStack";
 
 type GoToScreenProps = {
     screen: keyof RootStackProps;
+    params?: any;
     children: React.ReactNode;
     className?: string;
 };
 
-export default function GoToScreen({ screen, children, className }: GoToScreenProps) {
-    const navigation =
-        useNavigation<any>();
+export default function GoToScreen({ screen, params, children, className }: GoToScreenProps) {
+    const navigation = useNavigation<any>();
+
+    function handlePress() {
+        console.log("🚀 [NAVIGATE]", { screen, params });
+
+        navigation.getParent()?.navigate(screen, params);
+    }
 
     return (
         <TouchableOpacity
             className={className}
-            onPress={() => navigation.navigate(screen)}
+            onPress={handlePress}
         >
             {children}
         </TouchableOpacity>
